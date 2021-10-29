36099b084a39086121fb3ab5adc6fcbd31a39abb
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-10-29 00:04:41 -0700

    toolbar basics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Toolbar from './components/toolbar';
 import "./style/index.scss";
 
 function App() {
   return (
     <div className="App">
+      <Toolbar/>
       <h1>Test</h1>
     </div>
   );
--- src/components/toolbar/index.js
+++ src/components/toolbar/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import style from "./index.module.scss";
 
 export default class Toolbar extends Component {
   render() {
     return (
-      <div>
-        
+      <div className={style.toolbarContainer}>
+        <div className={style.appLogo}></div>
+        <div className={style.projectDeets}>
+          <div className={style.projectTitle}>Propa</div>
+          <div className={style.settings}></div>
+        </div>
       </div>
     );
   }

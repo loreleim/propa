e5d1124ab3f2609bc2156e642ae17c32b2ce0a37
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-10-29 00:15:10 -0700

    working toolbar

--- src/components/toolbar/index.js
+++ src/components/toolbar/index.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import style from "./index.module.scss";
+import store from "../../database";
 
 export default class Toolbar extends Component {
   render() {
     return (
       <div className={style.toolbarContainer}>
-        <div className={style.appLogo}></div>
-        <div className={style.projectDeets}>
-          <div className={style.projectTitle}>Propa</div>
-          <div className={style.settings}></div>
+
+        {/* Section 1 */}
+        <div className={style.sectionOne}>
+          <div className={style.appLogo}></div>
+
+          <div className={style.projectDeets}>
+            <input className={style.projectTitle} placeholder="Propa"/>
+
+            {/* Toolbar Settings */}
+            <div className={style.settings}>
+              {store.settings.map((x) => (
+                <div key={x + "-file"} id="file-menu">{x}</div>
+              ))}
+            </div>
+          </div>
+
         </div>
+
+        {/* Section 2 */}
+        <div className={style.sectionTwo}>
+          <div className={style.appLogo}></div>
+        </div>
+
       </div>
     );
   }
